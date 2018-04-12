4ed725721967eab1946c261baf0c746e606565c8
BowersMatthew <bowersma@mymail.vcu.edu>
2018-04-11 23:12:02 -0400

    moved address and to below the circles

--- kate-bowers-md/src/components/common/footer.js
+++ kate-bowers-md/src/components/common/footer.js
@@ -3,10 +3,11 @@ import FooterLinks from './nav/footer-links';
 
 export const Footer = () => {
     return (
-        <div className="page-footer-image">
-            <div className="page-footer-text">Firefly Pediatrics<br/>2937 Fox Trace Ln<br/> Midlothian, VA 23112
+        <div>
+            <div className="page-footer-image"></div>
+            <div className="page-footer-text">Firefly Pediatrics<br />2937 Fox Trace Ln<br /> Midlothian, VA 23112
             <FooterLinks />
-            </div>
+                </div>
         </div>
     )
 }
