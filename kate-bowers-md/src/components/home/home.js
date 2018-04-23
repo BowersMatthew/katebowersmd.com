0ff89925d0b0dbfde38dd2fa94577f5a7e39cfa8
BowersMatthew <bowersma@mymail.vcu.edu>
2018-04-22 22:11:25 -0400

    adding practice name to splash screen

--- kate-bowers-md/src/components/home/home.js
+++ kate-bowers-md/src/components/home/home.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
     render() {
-        return <section className='home' alt='Child Releasing Fireflies into Night Sky'></section>
+        return (
+            <section className='home' alt='Child Releasing Fireflies into Night Sky'>
+                <div className='home-title'>
+                    <h1 className="App-title">Firefly</h1>
+                    <br />
+                    <h1 className="App-sub-title">Pediatrics</h1>
+                </div>
+            </section>
+        )
     }
 }
 
