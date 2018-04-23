8ccea15954e70d1798b6825bbb3bd272291fc013
BowersMatthew <bowersma@mymail.vcu.edu>
2018-04-22 22:12:25 -0400

    vaccines-and-autism page

---
+++ kate-bowers-md/src/components/resources/parenting/vaccines-autism.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import VaccinePic from '../../../resources/vaccines-and-autism.jpg';
+
+export function VaccineAutism() {
+    return (
+        <section className="vaccine-autism">
+        <h3>Vaccines and Autism</h3>
+        <p>An enormous volume of scientific data has been amassed in order to be certain that there was no link between vaccines and autism. This infographic does a great job of clearly indicating several of these studies, as well as what a large number of children were studied in the quest to answer this question.</p>
+        <img className='full-width-image' src={VaccinePic}
+            alt='THE SCIENCE FACTS ABOUT AUTISM AND VACCINES What started the rumors? 
+            1998: Lancet published a paper by Dr. Andrew Wakefield it was a dramatic study that found a connection between autism and vaccines The Study Had Some Problems: Not based on statistics No control group
+It relied on people’s memories
+Made vague conclusions that weren’t statistically valid
+No Link was Found, so people started investigating his claims
+
+Following Dr. Wakefield’s study, here’s what other more rigorous studies found:
+
+1999: A study of 500 children no connection was found
+2001: A study of 10,000 children still found no connection
+2002: A study from Denmark of 537,000 children found no connection
+2002: A study from Finland of 535,000 children. Guess what? No connection
+“They had conducted invasive investigations on the children without obtaining the necessary ethical clearances… picked and chose data that suited their case; they falsified facts.”
+
+2004: Lancet released a statement refuting the original findings
+“They had conducted invasive investigations on the children without obtaining the necessary ethical clearances… picked and chose data that suited their case; they falsified facts.”
+2005: A review of 31 studies covering more than 10,000,000 children, also found no connection
+2012: A review of 27 cohort studies, 17 case control studies, 6 self- controlled case series studies, 5 time series trials, 2 ecological studies, 1 case cross-over trial covering over 14,700,000 children
+No link to autism was found in ANY case, in all of the studies.
+
+VACCINE VILIFICATION SURVIVES
+1/4 of U.S. parents U.S. parents believe some vaccines cause autism in healthy children
+1.8% of parents opt out of vaccines for religious or philosophical reasons
+There have been 0 credible studies linking vaccines to autism
+Recently an anti-vaccine religious community has seen measles outbreaks
+ALTHOUGH DECLARED ERADICATED IN 2000…
+France reported a massive measles outbreak with nearly 15,000 cases in 2011
+The U.K. reported more than 2,000 measles cases in 2012
+BEFORE WIDESPREAD VACCINATIONS OF BABIES
+in 1980, 2.6 million deaths from measles
+in 2000 562,400 deaths 72% of babies vaccinated
+in 2012, 122,000 deaths 84% of babies vaccinated
+IN THE UNITED STATES, WHOOPING COUGH SHOT UP IN 2012 TO NEARLY 50,000 CASES
+A new study concluded that vaccine refusals were largely to blame for a 2010 outbreak of whooping cough in California.
+
+A new study concluded that vaccine refusals were largely to blame for a 2010 outbreak of whooping cough in California.
+
+It is clear that immunization protects children from disease and saves lives. Outbreaks of many deadly diseases are on the decline globally, thanks to vaccinations. Immunizations can help eradicate many dangerous childhood diseases. Just like Smallpox, they too can be wiped off the face of the Earth. In the U.S., we almost had Whooping Cough beat in the 80s — then Dr. Wakefield’s fabulously flawed paper was published:
+
+1960s – 150,000 cases of whooping cough
+1960s – Widespread vaccinations introduced
+1970s – 5,000 cases of whooping cough
+1980s – 2,900 cases of whooping cough
+1998 – Dr. Wakefield’s paper published
+2004 – 26,000 cases of whooping cough
+2012 – 50,000 cases of whooping cough
+COMMON VACCINE MYTHS
+Vaccines are ridden with toxic chemicals that can harm children
+Thimersol, the chemical being referenced, does contain mercury. However, thimersol has been removed from scheduled vaccines and only resides in the seasonal flu vaccine.
+The decision to not vaccinate my child only affects my child
+Un-vaccinated children who contract a disease can infect infants yet to be inoculated, the small percentage of people whose vaccines did not take, and people with compromised immune systems.
+Receiving too many vaccines at once can override a baby’s immune system
+Baby’s immune systems are strong enough to defend from the day to day viruses and bacteria with which they come in contact; they can also handle the vaccines. Remember, vaccines use deactivated viruses in their ingredients.
+Drug companies just do it to make profits
+According to the WHO, estimated 2013 global revenues for all vaccines is around $24 billion, which only accounts for approximately 2 – 3% of the total pharmaceuticals market.
+VACCINES WORK!
+Positive effects of vaccines:
+
+Helped eradicate Smallpox
+Save about 8 million lives every year
+Significantly reduce disease in the world
+New and underutilized vaccines could avert nearly 4 million deaths of children under the age 5 by 2015
+SOURCES:
+
+http://www.bmj.com/content/342/bmj.c7452
+http://www.cdc.gov/pertussis/outbreaks/trends.html
+http://www.cdc.gov/vaccines/pubs/pinkbook/pert.html
+https://www.dosomething.org/tipsandtools/11-facts-about-vaccines
+http://www.npr.org/blogs/health/2010/02/lancet_wakefield_autism_mmr_au.html
+http://www.npr.org/blogs/health/2010/02/lancet_wakefield_autism_mmr_au.html
+http://skepticalraptor.com/blog/wp-content/uploads/2013/05/Vaccine-Market-Value.pdf
+http://www.theverge.com/2013/10/21/4767530/vaccine-deniers-inside-the-dumb-dangerous-new-fad
+http://www.nbcnews.com/id/35638229/ns/health-childrens_health/t/parents-thinks-shots-cause-autism
+http://upworthy.com/got-3-minutes-heres-everything-you-need-to-know-about-vaccines-and-their-controversy
+http://www.slate.com/blogs/bad_astronomy/2013/08/26/antivax_communities_get_measles_outbreaks_linked_to_denial_of_vaccines.html
+http://www.slate.com/blogs/bad_astronomy/2014/04/24/global_vaccination_week_2014_worldwide_effort_to_increase_immunizations.html
+http://www.slate.com/articles/news_and_politics/jurisprudence/2013/08/anti_vaxxers_why_parents_who_don_t_vaccinate_their_ki'/>
+        </section>
+    )
+}
