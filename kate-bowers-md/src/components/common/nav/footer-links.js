a39e6a4f2b1aab8cc4c3b551f4a5c6f9d51e31ed
BowersMatthew <bowersma@mymail.vcu.edu>
2018-04-22 22:45:45 -0400

    removing some links from footer

--- kate-bowers-md/src/components/common/nav/footer-links.js
+++ kate-bowers-md/src/components/common/nav/footer-links.js
@@ -4,8 +4,6 @@ import {Link} from 'react-router-dom';
 export const FooterLinks = () => {
     return (
         <nav className="page-footer-nav">
-            <Link to='/home'>Home</Link>
-            <Link to='/sickChild'>Sick Child</Link>
             <Link to='/site-map'>Site Map</Link>
             <Link to='/contact'>Contact Us</Link>
             <Link to='/portal'>Portal</Link>
