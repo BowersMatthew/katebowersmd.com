ac50798450e7149fd8c9a861e7eb4625457d0414
BowersMatthew <bowersma@mymail.vcu.edu>
2018-09-20 21:20:28 -0400

    adding page tacking using react-ga

--- kate-bowers-md/src/root.js
+++ kate-bowers-md/src/root.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  BrowserRouter as Router,
+  Router,
   Route,
   Switch,
 } from 'react-router-dom';
@@ -23,33 +23,51 @@ import Privacy from './components/contact/privacy';
 import Portal from './components/portal';
 import { VaccineAutism } from './components/resources/parenting/vaccines-autism';
 import PatientForms from './components/resources/patient-forms/patient-forms';
+import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-118938675-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
-const Root = (props) => {
-  return (
-    <Router>
-      <App>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/newPatients' component={NewPatients} />
-          <Route path='/Insurance' component={Insurance} />
-          <Route exact path='/resources' component={Resources} />
-          <Route exact path='/resources/vaccines' component={Vaccines} />
-          <Route exact path='/resources/parenting' component={Parenting} />
-          <Route exact path='/resources/parenting/vaccines-autism' component={VaccineAutism} />
-          <Route exact path='/resources/development' component={Development} />
-          <Route exact path='/resources/sickChild' component={SickChild} />
-          <Route exact path='/resources/sickChild/medicationDosing' component={MedicationDosing} />
-          <Route exact path='/resources/mentalHealth' component={MentalHealth} />
-          <Route exact path='/resources/patient-forms' component={PatientForms} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/contact/privacy' component={Privacy} />
-          <Route path='/site-map' component={SiteMap} />
-          <Route path='/portal' component={Portal} />
-        </Switch>
-      </App>
-    </Router>
-  );
+const history = createHistory();
+history.listen((location, action) => {
+  ReactGA.set({page: location.pathname});
+  ReactGA.pageview(location.pathname);
+})
+
+class Root extends Component {
+
+componentWillMount() {
+  ReactGA.set({page: history.location.pathname});
+  ReactGA.pageview(history.location.pathname);
+}
+
+  render() {
+    return (
+      <Router history={history}>
+        <App>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/newPatients' component={NewPatients} />
+            <Route path='/Insurance' component={Insurance} />
+            <Route exact path='/resources' component={Resources} />
+            <Route exact path='/resources/vaccines' component={Vaccines} />
+            <Route exact path='/resources/parenting' component={Parenting} />
+            <Route exact path='/resources/parenting/vaccines-autism' component={VaccineAutism} />
+            <Route exact path='/resources/development' component={Development} />
+            <Route exact path='/resources/sickChild' component={SickChild} />
+            <Route exact path='/resources/sickChild/medicationDosing' component={MedicationDosing} />
+            <Route exact path='/resources/mentalHealth' component={MentalHealth} />
+            <Route exact path='/resources/patient-forms' component={PatientForms} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/contact/privacy' component={Privacy} />
+            <Route path='/site-map' component={SiteMap} />
+            <Route path='/portal' component={Portal} />
+          </Switch>
+        </App>
+      </Router>
+    );
+  }
 };
 
 export default Root;
